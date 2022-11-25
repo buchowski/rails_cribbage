3ee9f45ca53eebe199d666c2b4f534ee1e206999
buchowski <bucholz.adam@gmail.com>
2022-11-25 09:35:41 -0800

    Add serializeCribbageGame

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,46 @@
 class GamesController < ApplicationController
   def index
+    @games = Game.all
+  end
+  def create
+    creator_id = params["creator_id"]
+
+    game = CribbageGame::Game.new
+    serialized = serializeCribbageGame game
+
+    p "johnny", serialized
+
+  end
+
+  private
+
+  def safelyGetPlayerId player
+    if !player
+      return nil
+    end
+    player.id
+  end
+
+  def serializeCribbageGame game
+    player_one = game.players[0]
+    player_two = game.players[1]
+
+    {
+      "player_one_id" => player_one.id,
+      "player_two_id" => player_two.id,
+      "player_one_cards" => player_one.hand,
+      "player_two_cards" => player_two.hand,
+      "player_one_points" => player_one.total_score,
+      "player_two_points" => player_two.total_score,
+      "dealer_id" => safelyGetPlayerId(game.dealer),
+      "pile_cards" => game.pile,
+      "crib_cards" => game.crib,
+      "cut_card" => game.cut_card,
+      "whose_turn_id" => safelyGetPlayerId(game.whose_turn),
+      "current_fsm_state" => game.fsm.aasm.current_state,
+      "round" => game.round,
+      "points_to_win" => game.points_to_win,
+      "winner_id" => safelyGetPlayerId(game.winner)
+    }
   end
 end
