6efb305ce1015e136639a57eab36ed4f1047091a
buchowski <bucholz.adam@gmail.com>
2022-11-27 12:49:00 -0800

    Add join game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -7,12 +7,20 @@ class GamesController < ApplicationController
     @game = Game.new(creator_id)
 
     if @game.save
-      p "success: game was saved", @game
-
       redirect_to games_path
     else
       p "error: game failed to save", @game
     end
+  end
+  def update
+    game_id = params[:id]
+    type_of_update = params[:type_of_update]
+
+    case type_of_update
+    when "join_game" then join_game(game_id)
+    end
+
+    redirect_to games_path
   end
 
   def destroy
@@ -20,11 +28,24 @@ class GamesController < ApplicationController
     game = Game.find(game_id)
 
     if game.destroy
-      p "success: game was deleted", @game
-
       redirect_to games_path
     else
       p "error: game failed to delete", @game
+    end
+  end
+
+  private
+
+  def join_game(game_id)
+    game = Game.find(game_id)
+
+    if game.player_two_id.nil?
+      was_created_by_sally = game.player_one_id == 'sally'
+      game.player_two_id = was_created_by_sally ? 'hank' : 'sally'
+
+      if !game.save then throw "failed to join game" end
+    else
+      throw "you're not allowed to join. too many players already"
     end
   end
 end
