536be8114a1f54c7d1e874f98219c89af4e0ae91
buchowski <bucholz.adam@gmail.com>
2022-12-03 15:48:54 -0800

    Move join button to show page

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,6 +2,9 @@ class GamesController < ApplicationController
   def index
     @games = Game.all
   end
+  def show
+    @game = get_game()
+  end
   def create
     creator_id = params[:creator_id]
     @game = Game.new(creator_id)
@@ -13,23 +16,22 @@ class GamesController < ApplicationController
     end
   end
   def update
-    game_id = params[:id]
+    game = get_game()
     type_of_update = params[:type_of_update]
 
     begin
       case type_of_update
-      when "join_game" then join_game(game_id)
+      when "join_game" then join_game(game)
       end
     rescue => exception
       flash[:error_msg] = exception
     ensure
-      redirect_to games_path
+      redirect_to game_path
     end
   end
 
   def destroy
-    game_id = params[:id]
-    game = Game.find(game_id)
+    game = get_game()
 
     if game.destroy
       redirect_to games_path
@@ -40,12 +42,16 @@ class GamesController < ApplicationController
 
   private
 
-  def join_game(game_id)
-    game = Game.find(game_id)
+  def get_game
+    game_id = params[:id]
+    Game.find(game_id)
+  end
 
+  def join_game(game)
     if game.player_two_id.nil?
       was_created_by_sally = game.player_one_id == 'sally'
       game.player_two_id = was_created_by_sally ? 'hank' : 'sally'
+      game.current_fsm_state = 'waiting_to_start'
 
       if !game.save then throw "failed to join game" end
     else
