6b7ca3e9362a2c3ca4df62639ffc7b46804e5dc3
buchowski <bucholz.adam@gmail.com>
2022-12-20 18:57:08 -0800

    Convert game_model to game instance for update

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -9,23 +9,35 @@ class GamesController < ApplicationController
     creator_id = params[:creator_id]
     @game = Game.new(creator_id)
 
-    if @game.save
-      redirect_to games_path
-    else
-      p "error: game failed to save", @game
+    if !@game.save
+      flash[:error_msg] = "error: failed to save game"
     end
+    redirect_to games_path
   end
   def update
-    game = get_game()
+    game_model = get_game()
     type_of_update = params[:type_of_update]
 
     begin
-      case type_of_update
-      when "join_game" then join_game(game)
-      when "start_game" then start_game(game)
+      if type_of_update == "join_game"
+        join_game(game_model)
+      elsif type_of_update == "start_game"
+        start_game(game_model)
+      else
+        game = get_cribbage_game(game_model)
+
+        case type_of_update
+        when "cut_for_deal"
+          game.cut_for_deal()
+        when "deal"
+          game.deal()
+        end
+
+        game_model.update(Game.adapt_to_active_record(game))
       end
     rescue => exception
-      flash[:error_msg] = exception
+      # truncate exception.message to prevent cookieoverflow
+      flash[:error_msg] = exception.message[0, 100]
     ensure
       redirect_to game_path
     end
@@ -34,11 +46,10 @@ class GamesController < ApplicationController
   def destroy
     game = get_game()
 
-    if game.destroy
-      redirect_to games_path
-    else
-      p "error: game failed to delete", @game
+    if !game.destroy
+      flash[:error_msg] = "error: failed to delete game"
     end
+    redirect_to games_path
   end
 
   private
@@ -61,12 +72,16 @@ class GamesController < ApplicationController
   end
 
   def start_game(game)
-    game.current_fsm_state = Game.getFsmStartState()
-
-    if game.current_fsm_state != :waiting_to_start
+    if game.current_fsm_state.to_sym != :waiting_to_start
       throw "this game is either not ready to start or has been started already"
     end
 
+    game.current_fsm_state = :cutting_for_deal
+
     if !game.save then throw "unable to start game" end
+  end
+
+  def get_cribbage_game(game_model)
+    Game.adapt_to_cribbage_game(game_model)
   end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,12 +1,12 @@
 class Game < ApplicationRecord
-  serialize :player_one_cards, Array
-  serialize :player_two_cards, Array
+  serialize :player_one_cards, Hash
+  serialize :player_two_cards, Hash
   serialize :pile_cards, Array
   serialize :crib_cards, Array
 
   def initialize(creator_id)
     cribbage_game = CribbageGame::Game.new
-    adapted_game = adaptCribbageGame(cribbage_game)
+    adapted_game = self.class.adapt_to_active_record(cribbage_game)
 
     adapted_game[:player_one_id] = creator_id
     adapted_game[:player_two_id] = nil
@@ -15,17 +15,13 @@ class Game < ApplicationRecord
     super(adapted_game);
   end
 
-  def self.getFsmStartState
-    CribbageGame::Game.new.fsm.aasm.current_state
-  end
-
   private
 
-  def safelyGetPlayerId player
+  def self.safely_get_player_id player
     player ? player.id : nil
   end
 
-  def adaptCribbageGame game
+  def self.adapt_to_active_record(game)
     player_one = game.players[0]
     player_two = game.players[1]
 
@@ -36,15 +32,45 @@ class Game < ApplicationRecord
       "player_two_cards" => player_two.hand,
       "player_one_points" => player_one.total_score,
       "player_two_points" => player_two.total_score,
-      "dealer_id" => safelyGetPlayerId(game.dealer),
+      "dealer_id" => self.safely_get_player_id(game.dealer),
       "pile_cards" => game.pile,
       "crib_cards" => game.crib,
       "cut_card" => game.cut_card,
-      "whose_turn_id" => safelyGetPlayerId(game.whose_turn),
+      "whose_turn_id" => self.safely_get_player_id(game.whose_turn),
       "current_fsm_state" => game.fsm.aasm.current_state,
       "round" => game.round,
       "points_to_win" => game.points_to_win,
-      "winner_id" => safelyGetPlayerId(game.winner)
+      "winner_id" => self.safely_get_player_id(game.winner)
     }
+  end
+
+  def self.adapt_to_cribbage_game(game_model)
+    game = CribbageGame::Game.new
+    player_one = game.players[0]
+    player_two = game.players[1]
+
+    player_one.id = game_model.player_one_id
+    player_one.hand = game_model.player_one_cards
+    player_one.total_score = game_model.player_one_points
+    player_two.id = game_model.player_two_id
+    player_two.hand = game_model.player_two_cards
+    player_two.total_score = game_model.player_two_points
+
+    is_player_one_dealer = game_model.dealer_id == game_model.player_one_id
+    game.dealer = is_player_one_dealer ? player_one : player_two
+    is_player_one_turn = game_model.whose_turn_id == game_model.player_one_id
+    game.whose_turn = is_player_one_turn ? player_one : player_two
+    # is_player_one_winner = game_model.winner_id == game_model.player_one_id
+    # game.winner = is_player_one_winner ? player_one : player_two
+
+    game.fsm.aasm.current_state = game_model.current_fsm_state.to_sym
+    game.round = game_model.round
+    game.points_to_win = game_model.points_to_win
+
+    game.pile = game_model.pile_cards
+    game.crib = game_model.crib_cards
+    game.cut_card = game_model.cut_card
+
+    game
   end
 end
