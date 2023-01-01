c3bb96b105cbe4aa10925153e8237dc7c4f371b3
buchowski <bucholz.adam@gmail.com>
2022-12-31 21:27:58 -0800

    Add games model & controller tests

--- test/controllers/games_controller_test.rb
+++ test/controllers/games_controller_test.rb
@@ -5,4 +5,26 @@ class GamesControllerTest < ActionDispatch::IntegrationTest
     get games_path
     assert_response :success
   end
+
+  test "should display error_msg when #create is called without player_name" do
+    cereal_uri = "/cereal-sweepstakes/how-to-win"
+
+    assert_no_difference("Game.count") do
+      post games_path, headers: { "HTTP_REFERER" => cereal_uri }
+    end
+
+    assert_redirected_to cereal_uri
+    assert_equal flash[:error_msg], "you must include your player_name in the request"
+  end
+
+  test "should create new game if player_name is provided" do
+    cereal_uri = "/cereal-sweepstakes/how-to-win"
+
+    assert_difference("Game.count") do
+      post games_path, headers: { "HTTP_REFERER" => cereal_uri }, params: { player_name: "judith" }
+    end
+
+    assert_redirected_to games_path
+    assert_nil flash[:error_msg]
+  end
 end
--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,7 +1,31 @@
 require "test_helper"
 
 class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should not initialize without a creator_id" do
+    game = Game.new()
+    assert_not(game.save())
+  end
+
+  test "should convert a CribbageGame::Game instance to hash" do
+    game = CribbageGame::Game.new()
+
+    game.players[0].id = "player_uno"
+    game.players[1].id = "player_dos"
+
+    game_hash = Game.adapt_to_active_record(game)
+
+    assert_equal(game_hash["player_one_id"], "player_uno")
+    assert_equal(game_hash["player_two_id"], "player_dos")
+    assert_equal(game_hash["current_fsm_state"], :cutting_for_deal)
+  end
+
+  test "should convert an ActiveRecord Game model to a CribbageGame::Game instance" do
+    game_model = Game.new("tony_tiger")
+
+    game = Game.adapt_to_cribbage_game(game_model)
+
+    assert_equal(game.players[0].id, "tony_tiger")
+    assert_nil(game.players[1].id)
+    assert_equal(game.fsm.aasm.current_state, :waiting_for_player_two)
+  end
 end
