3c641996941c48f4cdc5377a24e452ef5283bcee
buchowski <bucholz.adam@gmail.com>
2023-05-20 21:49:10 -0700

    Throw error if user/game.player mismatch

--- app/models/game.rb
+++ app/models/game.rb
@@ -18,8 +18,8 @@ class Game < ApplicationRecord
 
   private
 
-  def self.safely_get_player_id player
-    player ? player.id : nil
+  def self.safely_get_id entity
+    entity ? entity.id : nil
   end
 
   def self.adapt_to_active_record(game)
@@ -33,15 +33,15 @@ class Game < ApplicationRecord
       "player_two_cards" => player_two.hand,
       "player_one_points" => player_one.total_score,
       "player_two_points" => player_two.total_score,
-      "dealer_id" => self.safely_get_player_id(game.dealer),
+      "dealer_id" => self.safely_get_id(game.dealer),
       "pile_cards" => game.pile,
       "crib_cards" => game.crib,
       "cut_card" => game.cut_card,
-      "whose_turn_id" => self.safely_get_player_id(game.whose_turn),
+      "whose_turn_id" => self.safely_get_id(game.whose_turn),
       "current_fsm_state" => game.fsm.aasm.current_state,
       "round" => game.round,
       "points_to_win" => game.points_to_win,
-      "winner_id" => self.safely_get_player_id(game.winner)
+      "winner_id" => self.safely_get_id(game.winner)
     }
   end
 
@@ -51,10 +51,18 @@ class Game < ApplicationRecord
     return winner_id == player_one.id ? player_one : player_two
   end
 
+  def self.are_users_valid(game_model, user_one, user_two)
+    return false if safely_get_id(user_one) != game_model.player_one_id
+    return false if safely_get_id(user_two) != game_model.player_two_id
+    return true
+  end
+
   def self.adapt_to_cribbage_game(game_model, user_one, user_two)
     game = CribbageGame::Game.new
     player_one = game.players[0]
     player_two = game.players[1]
+
+    throw "users do not match game.players" if !self.are_users_valid(game_model, user_one, user_two)
 
     player_one.id = game_model.player_one_id
     player_one.hand = game_model.player_one_cards
