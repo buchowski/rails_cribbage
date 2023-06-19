eec343c370b93de4b19b62e4d3f97a1bce403aa7
buchowski <bucholz.adam@gmail.com>
2023-06-18 21:33:18 -0700

    Enable user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
+
   def new
   end
 
   def create
+    if !passwords_valid?(params)
+      flash[:error_msg] = "Sorry, your passwords do not match"
+      redirect_back(fallback_location: root_path)
+      return
+    end
+
     user = User.new(name: params[:name], email: params[:email])
+
+    if !params[:password].empty?
+      user.password_digest = BCrypt::Password.create(params[:password])
+    end
 
     if user.save!
       session[:user_id] = user.id
@@ -17,5 +28,14 @@ class UsersController < ApplicationController
     user = User.find(params[:user_id])
     session[:user_id] = user.id
     redirect_to games_path
+  end
+
+  private
+
+  def passwords_valid?(params)
+    return true if params[:password].empty? && params[:password_confirm].empty?
+
+    # TODO add length requirement
+    params[:password] == params[:password_confirm]
   end
 end
