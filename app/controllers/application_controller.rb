3bdfc8330cbb576c35c37843dee40e95bc9ee099
buchowski <bucholz.adam@gmail.com>
2024-02-04 13:13:04 -0800

    Use devise current_user to get @user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :get_user
 
   def get_user
-    @user = User.find_by_id(session[:user_id]) || AnonUser.new
+    @user = current_user || AnonUser.new
     @app = AppPresenter.new(@user)
   end
 end
