d93d42f51a170cd7634cd0388d90c8b57093ab7e
buchowski <bucholz.adam@gmail.com>
2024-03-18 21:02:09 -0700

    Fix sass hot reload

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,6 +63,9 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
+  # 3/18/24 added this to fix sass hot reload error
+  config.assets.debug = true
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
