86e046a5abc6dce29140c1d2dd79165dc47d6119
buchowski <bucholz.adam@gmail.com>
2024-04-14 12:33:28 -0700

    Move broadcast_to_guests inside included

--- app/controllers/concerns/broadcast.rb
+++ app/controllers/concerns/broadcast.rb
@@ -15,19 +15,19 @@ module Broadcast
         }
       )
     end
-  end
 
-  def broadcast_to_guests(guest_gvm, anon_user)
-    # if any guests are watching the game, update their views
-    guest_stream_id = guest_gvm.get_stream_id_for_user(anon_user)
-    Turbo::StreamsChannel.broadcast_render_to(
-      guest_stream_id,
-      partial: "games/update",
-      locals: {
-        gvm: guest_gvm,
-        pre_bot_update_html_string: nil,
-        app: AppPresenter.new(anon_user)
-      }
-    )
+    def broadcast_to_guests(guest_gvm, anon_user)
+      # if any guests are watching the game, update their views
+      guest_stream_id = guest_gvm.get_stream_id_for_user(anon_user)
+      Turbo::StreamsChannel.broadcast_render_to(
+        guest_stream_id,
+        partial: "games/update",
+        locals: {
+          gvm: guest_gvm,
+          pre_bot_update_html_string: nil,
+          app: AppPresenter.new(anon_user)
+        }
+      )
+    end
   end
 end
