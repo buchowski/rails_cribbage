fe6860af94b5409a15563b18644ca19ef6592981
buchowski <bucholz.adam@gmail.com>
2024-04-28 15:23:29 -0700

    Fix undefined devise var bug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,11 @@ class ApplicationController < ActionController::Base
   before_action :get_user
 
   def get_user
-    @user = current_user || AnonUser.new
+    if ENV["RAILS_ENV"] == "test"
+      @user = current_user || AnonUser.new
+    else
+      @user = AnonUser.new
+    end
     @app = AppPresenter.new(@user)
   end
 end
